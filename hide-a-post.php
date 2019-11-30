77297f7dfb07d9b4a077a56375ee24985f923ad6
Joe Rybacek <joe.rybacek@outlook.com>
2019-11-30 17:13:33 -0600

    Rename EMA to HAP

--- hide-a-post.php
+++ hide-a-post.php
@@ -5,7 +5,7 @@
  * Description: WordPress plugin that hides posts on the site, search, and WordPress admin from non-Administrators using one or more categories.
  * Version: 0.1
  * Author: Joe Rybacek
- * Author URI: http://www.rybacek-consulting.com
+ * Author URI: https://github.com/jrybacek
  */
 
 // Make sure we don't expose any info if called directly
@@ -14,32 +14,33 @@ if(!function_exists('add_action')) {
   exit;
 }
 
+define('HAP_DISPLAYNAME', 'Hide A Post');
 define('HAP_FILE', plugin_basename(__FILE__));
-define('HAP_PATH', 'options-general.php?page=ema');
-$ema_options = get_option('ema');
+define('HAP_PATH', 'options-general.php?page=hap');
+$hap_options = get_option('hap');
 
-function ema_options_page() {
-  global $ema_options;
+function hap_options_page() {
+  global $hap_options;
   ?>
 	<div class="wrap">
 		<?php screen_icon(); ?>
-		<h2><?php _e( 'Easy Multi-User Authoring', 'ema' ); ?></h2>
+		<h2><?php _e(HAP_DISPLAYNAME, 'hap'); ?></h2>
 		<form method="POST" action="options.php">
-			<?php settings_fields( 'ema_options' ); ?>
+			<?php settings_fields('hap_options'); ?>
 			<table class="form-table">
 				<tbody>
 					<tr valign="top">
-						<th scope="row"><?php _e( 'What category or categories should be hidden from the editors?', 'ema' ); ?></th>
+						<th scope="row"><?php _e('What category or categories should be hidden from the editors?', 'hap'); ?></th>
 						<td>
 							<p>
 								<?php
-									if ( $ema_options['categories'] ) {
-										$ema_options['categories'] = ema_options_categories_to_name($ema_options['categories']);
+									if ( $hap_options['categories'] ) {
+										$hap_options['categories'] = hap_options_categories_to_name($hap_options['categories']);
 									} else {
-										$ema_options['categories'] = __( '', 'ema' );
+										$hap_options['categories'] = __( '', 'hap' );
 									}
 								?>
-								<input type="text" id="ema-categories" name="ema[categories]" width="15" value="<?php echo $ema_options['categories']; ?>" /><br />
+								<input type="text" id="hap-categories" name="hap[categories]" width="15" value="<?php echo $hap_options['categories']; ?>" /><br />
 							</p>
 						</td>
 					</tr>
@@ -52,65 +53,65 @@ function ema_options_page() {
 	<?php
 }
 
-function ema_menu() {
-	add_submenu_page('options-general.php', __('Easy Multi-User Authoring', 'ema'), __('Easy Multi-User Authoring', 'ema'), 'administrator', 'ema', 'ema_options_page');
+function hap_menu() {
+	add_submenu_page('options-general.php', __(HAP_DISPLAYNAME, 'hap'), __(HAP_DISPLAYNAME, 'hap'), 'administrator', 'hap', 'hap_options_page');
 }
-add_action('admin_menu', 'ema_menu');
+add_action('admin_menu', 'hap_menu');
 
-function ema_admin_init() {
-	register_setting('ema_options', 'ema', 'ema_validate');
+function hap_admin_init() {
+	register_setting('hap_options', 'hap', 'hap_validate');
 }
-add_action('admin_init', 'ema_admin_init');
+add_action('admin_init', 'hap_admin_init');
 
-function ema_admin_notices() {
+function hap_admin_notices() {
 	global $pagenow;
-	if($pagenow == 'plugins.php' && current_user_can('administrator') && !ema_configured()) {
+	if($pagenow == 'plugins.php' && current_user_can('administrator') && !hap_configured()) {
 		?>
 		<div class="error">
-			<p><?php printf( __( 'Please configure the categories in the <a href="%s">settings menu</a> in order to start using Easy Multi-User Authoring plugin.', 'ema' ), esc_url( HAP_PATH ) ); ?></p>
+			<p><?php printf( __( 'Please configure the categories in the <a href="%s">settings menu</a> in order to start using ' . HAP_DISPLAYNAME . ' plugin.', 'hap' ), esc_url( HAP_PATH ) ); ?></p>
 		</div>
 		<?php
 	}
 }
 
-function ema_configured() {
-  global $ema_options;
+function hap_configured() {
+  global $hap_options;
 	$configured = true;
-	if(count($ema_options['categories']) == 0) {
+	if(count($hap_options['categories']) == 0) {
 		$configured = false;
 	}
 	return $configured;
 }
 
-function ema_activation() {
+function hap_activation() {
 	// Set default option values
 	$default = [ 'categories' => [] ];
 	// Write those values to the database
-	add_option('ema', $default);
+	add_option('hap', $default);
 }
 
-function ema_deactivation() {
+function hap_deactivation() {
 	// Remove options from the database
-	delete_option('ema');
+	delete_option('hap');
 }
 
 if( is_admin() ) {
-	register_activation_hook(__FILE__, 'ema_activation');
-	register_deactivation_hook(__FILE__, 'ema_deactivation');
-	add_action('admin_notices', 'ema_admin_notices');
+	register_activation_hook(__FILE__, 'hap_activation');
+	register_deactivation_hook(__FILE__, 'hap_deactivation');
+	add_action('admin_notices', 'hap_admin_notices');
 }
 
-function ema_action_links($links, $file) {
+function hap_action_links($links, $file) {
 	if( $file === HAP_FILE && current_user_can('manage_options')) {
-		$settings = '<a href="' . admin_url(HAP_PATH) . '">' . esc_html__('Settings', 'ema') . '</a>';
+		$settings = '<a href="' . admin_url(HAP_PATH) . '">' . esc_html__('Settings', 'hap') . '</a>';
 		array_unshift($links, $settings);
 	}
 	return $links;
 }
 
-add_filter('plugin_action_links', 'ema_action_links', 10, 2);
+add_filter('plugin_action_links', 'hap_action_links', 10, 2);
 
-function ema_validate($input) {
+function hap_validate($input) {
 	$categories = [];
 	$categories_valid = true;
 	foreach( explode( ',', $input['categories'] ) as $category ) {
@@ -128,7 +129,7 @@ function ema_validate($input) {
 	return $input;
 }
 
-function ema_options_categories_to_name($input) {
+function hap_options_categories_to_name($input) {
 	foreach( $input as &$category) {
 		$category_name = get_cat_name($category);
 		if ( !$category_name == '') {
@@ -138,12 +139,12 @@ function ema_options_categories_to_name($input) {
 	return implode( ', ', $input );
 }
 
-add_filter( 'posts_join', 'ema_posts_join', 10, 2 );
-add_filter( 'posts_where', 'ema_posts_where', 10, 2 );
+add_filter( 'posts_join', 'hap_posts_join', 10, 2 );
+add_filter( 'posts_where', 'hap_posts_where', 10, 2 );
 
-function ema_posts_join($join, $query) {
-	global $ema_options, $pagenow, $wpdb;
-	if(count($ema_options['categories']) > 0) {
+function hap_posts_join($join, $query) {
+	global $hap_options, $pagenow, $wpdb;
+	if(count($hap_options['categories']) > 0) {
 		$user_id = get_current_user_id();
 		$user_meta = get_userdata($user_id);
 		$user_roles = $user_meta->roles;
@@ -156,9 +157,9 @@ function ema_posts_join($join, $query) {
 	return $join;
 }
 
-function ema_posts_where($where, $query) {
-  global $ema_options, $pagenow;
-	if(count($ema_options['categories']) > 0) {
+function hap_posts_where($where, $query) {
+  global $hap_options, $pagenow;
+	if(count($hap_options['categories']) > 0) {
 		$user_id = get_current_user_id();
 		$user_meta = get_userdata($user_id);
 		$user_roles = $user_meta->roles;
@@ -171,7 +172,7 @@ LEFT JOIN wp_term_relationships as wtr ON (wp_posts.ID = wtr.object_id)
 LEFT JOIN wp_term_taxonomy as wtt ON (wtr.term_taxonomy_id = wtt.term_taxonomy_id)
 LEFT JOIN wp_terms as wt ON(wtt.term_id = wt.term_id)
 WHERE 1=1 AND wp_posts.post_type = 'post' AND (wp_posts.post_status = 'publish' OR wp_posts.post_status = 'future' OR wp_posts.post_status = 'draft' OR wp_posts.post_status = 'pending' OR wp_posts.post_status = 'private')
-AND wtt.taxonomy IN ('category') AND wt.term_id IN (" . implode( ',', $ema_options['categories']) . "))";
+AND wtt.taxonomy IN ('category') AND wt.term_id IN (" . implode( ',', $hap_options['categories']) . "))";
 		}
 	}
 	return $where;
